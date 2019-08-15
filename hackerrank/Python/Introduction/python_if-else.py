3b13ea00d7cbe044847bcda4f393ab43082ac002
Ajay-2007 <32496279+Ajay-2007@users.noreply.github.com>
2019-08-15 16:42:43 +0530

    Create python_if-else.py

---
+++ hackerrank/Python/Introduction/python_if-else.py
@@ -0,0 +1,28 @@
+# Problem Link
+# https://www.hackerrank.com/challenges/py-if-else
+
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+def main(n):
+    if n%2:
+        print("Weird")
+    elif  2 <= n <= 5:
+        print("Not Weird")
+    elif 6<= n <= 20:
+        print("Weird")
+    else :
+        print("Not Weird")
+
+
+
+if __name__ == '__main__':
+    n = int(input().strip())
+    main(n)
